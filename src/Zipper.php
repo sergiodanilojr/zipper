ca11738b09b409731355408cd0dfef917a72c710
sergiodanilojr <sergiodanilojr@hotmail.com>
2020-02-21 22:18:14 -0300

    Fixing The Download Method

--- src/Zipper.php
+++ src/Zipper.php
@@ -70,7 +70,7 @@ class Zipper
         if (!is_null($dataFile = $this->dataFile($file))) {
 
             $path = $this->destiny($file);
-           // $this->zip = new \ZipArchive();
+            // $this->zip = new \ZipArchive();
             if ($this->zip->open($path, $this->zip::CREATE) === true) {
                 $bool = $this->zip->addFile($file, $dataFile->basename);
                 $this->zip->close();
@@ -136,7 +136,7 @@ class Zipper
                 $this->zip->close();
             }
             $saved = $destiny . "/" . $dataFile->basename;
-            return $this->checkDir($saved);
+            return $saved;
         }
         $this->message = "Algo Impossibilitou a Extração do Arquivo!";
         return null;
