3a1d0082de4afcc07a07959e2d1efa36c24a1f9e
sergiodanilojr <sergiodanilojr@hotmail.com>
2020-02-21 22:08:16 -0300

    Fixing The Download Method

--- src/Zipper.php
+++ src/Zipper.php
@@ -70,7 +70,7 @@ class Zipper
         if (!is_null($dataFile = $this->dataFile($file))) {
 
             $path = $this->destiny($file);
-            $this->zip = new \ZipArchive();
+           // $this->zip = new \ZipArchive();
             if ($this->zip->open($path, $this->zip::CREATE) === true) {
                 $bool = $this->zip->addFile($file, $dataFile->basename);
                 $this->zip->close();
